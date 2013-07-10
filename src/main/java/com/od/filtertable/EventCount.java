eec1f397538cfcd02c44db54b69575b1532e2b7d
Nick Ebbutt <nick@objectdefinitions.com>
2013-07-10 18:37:36 +0100

    make filter counts publically available

--- src/main/java/com/od/filtertable/EventCount.java
+++ src/main/java/com/od/filtertable/EventCount.java
@@ -12,4 +12,35 @@ public class EventCount {
     int updateEvents;
     int rowInsertEvents;
     int rowDeleteEvents;
+
+    public int getDataChangeEvents() {
+        return dataChangeEvents;
+    }
+
+    public int getRowDeleteEvents() {
+        return rowDeleteEvents;
+    }
+
+    public int getRowInsertEvents() {
+        return rowInsertEvents;
+    }
+
+    public int getStructureChangeEvents() {
+        return structureChangeEvents;
+    }
+
+    public int getUpdateEvents() {
+        return updateEvents;
+    }
+    
+    public String toString() {
+        StringBuilder sb = new StringBuilder("EventCount-[");
+        sb.append("structureChange:").append(structureChangeEvents);
+        sb.append("dataChange:").append(dataChangeEvents);
+        sb.append("updates:").append(updateEvents);
+        sb.append("rowInsert").append(rowInsertEvents);
+        sb.append("rowDelete").append(rowDeleteEvents);
+        sb.append("]");
+        return sb.toString();
+    }
 }
